51125b4c63904d01e2ffbb89c2985ab153820f67
Joshua Dyal <=>
2026-09-03 12:57:03 +0100

    add multiple JSON files, improve system prompt, and add response streaming

--- main.py
+++ main.py
@@ -1,25 +1,135 @@
 # pip install -qU langchain langchain-ollama
 from langchain.agents import create_agent
-from vector import retriever
+from langchain_ollama import ChatOllama
+from langchain_core.utils.uuid import uuid7
+from langchain_core.runnables import Runnable
+from vector import (
+    incident_retriever,
+    incident_solution_retriever,
+    playbook_retriever,
+    policy_retriever,
+    security_history_retriever,
+)
 
 
-def get_weather(city: str) -> str:
-    """Get weather for a given city."""
-    return f"It's always sunny in {city}!"
+def get_incidents(query: str):
+    """Search the company's incident database for past cybersecurity incidents that match the query.
+
+    Args:
+        query: Search terms to look for
+    """
+    return incident_retriever.invoke(query)
 
 
-def get_incidents(question: str):
-    """Get previous cybersecurity incidents based on a question."""
-    return retriever.invoke(question)
+def get_incident_solutions(query: str):
+    """Search the company's incident solutions database for solutions to past cybersecurity incidents that match the query.
+
+    Args:
+        query: Search terms to look for"""
+    return incident_solution_retriever.invoke(query)
 
 
-agent = create_agent(
-    model="ollama:qwen3:0.6b",
-    tools=[get_weather, get_incidents],
-    system_prompt="You are a helpful cybersecurity assistant analyst.",
+def get_playbooks(query: str):
+    """Search the company playbooks database for playbook records matching the query. Playbooks list the steps to take when an incident occurs.
+
+    Args:
+        query: Search terms to look for"""
+    return playbook_retriever.invoke(query)
+
+
+def get_policies(query: str):
+    """Search the company policies database for records matching the query. Policies are set out by the company and must be followed at all times.
+
+    Args:
+        query: Search terms to look for"""
+    return policy_retriever.invoke(query)
+
+
+def get_security_history(query: str):
+    """Search the company security-history database for records matching the query. Historical records are split into quarters and summarise & total the number of incidents.
+
+    Args:
+        query: Search terms to look for"""
+    return security_history_retriever.invoke(query)
+
+
+llm = ChatOllama(model="qwen3:4b", num_ctx=8192)
+
+system_prompt = """
+    You are a cybersecurity assistant.
+
+    Your job is to help analyse and research security incidents, vulnerabilities,
+    security events, and answer cybersecurity questions.
+
+    You will be used by cybersecurity specialists.
+
+    Use the available tools when you need information that is not already
+    available in the conversation.
+
+    When answering questions about the company, prioritise information
+    retrieved from the company's internal knowledge base.
+
+    The internal knowledge base consists of:
+    - Incidents - past cybersecurity incidents that have occured
+    - Incident solutions - the solutions that resolved the incidents that have occured in the past
+    - Playbooks - company-specific guidance that should be followed in the event of a specific incident
+    - Policies - a separate database containing company-specific policies that must be followed at all times
+    - Security history - Quaterly history logs that include statistics and key observations.
+    
+    You have access to tools that let you search the internal knowledge base.
+
+    Always follow company policies.
+    
+    Do not invent company-specific facts, incidents, vulnerabilities,
+    policies, or procedures.
+
+    If you do not have enough information to answer confidently, say so.
+
+    Clearly distinguish between:
+    - Company-specific information
+    - General cybersecurity knowledge
+    - Information obtained from external sources
+
+    Provide technically accurate and practical answers.
+
+    You may use more than one tool per response.
+    You may use the same tool more than once, if necessary.
+"""
+
+agent: Runnable = create_agent(
+    model=llm,
+    tools=[
+        get_incidents,
+        get_incident_solutions,
+        get_playbooks,
+        get_policies,
+        get_security_history,
+    ],
+    system_prompt=system_prompt,
 )
 
 user_input = input("What is your input?: ")
 
-result = agent.invoke({"messages": [{"role": "user", "content": user_input}]})
-print(result["messages"][-1].content_blocks)
+# # SIMPLE OUTPUT
+# result = agent.invoke({"messages": [{"role": "user", "content": user_input}]})
+# print(result["messages"][-1].content_blocks[0]["text"])
+
+# # STREAMED OUTPUT
+config = {"configurable": {"thread_id": str(uuid7())}}
+stream = agent.stream_events(
+    {"messages": [{"role": "user", "content": user_input}]},
+    config=config,
+    version="v3",
+)
+
+for kind, item in stream.interleave("messages", "tool_calls"):
+    if kind == "messages":
+        for token in item.text:
+            print(token, end="", flush=True)
+    elif kind == "tool_calls":
+        print(f"\nTool call: {item.tool_name}({item.input})")
+        for delta in item.output_deltas:
+            print(delta, end="", flush=True)
+        print(f"\nTool result: {item.output}")
+
+final_state = stream.output
--- vector.py
+++ vector.py
@@ -11,18 +11,77 @@ embeddings = OllamaEmbeddings(model="mxbai-embed-large")
 db_location = "./chrome_langchain_db"
 add_documents = not os.path.exists(db_location)
 
+# Create the vector stores
+incident_store = Chroma(
+    collection_name="incidents",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+incident_solution_store = Chroma(
+    collection_name="incident_solutions",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+playbook_store = Chroma(
+    collection_name="playbooks",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+policy_store = Chroma(
+    collection_name="policies",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
+security_history_store = Chroma(
+    collection_name="security_history",
+    persist_directory=db_location,
+    embedding_function=embeddings,
+)
+
 # If we need to create the vectorDB, prepare the documents
 if add_documents:
-    documents = []
-    ids = []
+    # SET UP EMPTY LISTS FOR ALL VECTOR STORES
+    incident_documents = []
+    incident_ids = []
 
-    with open("incidents.json", "r", encoding="utf-8") as f:
+    incident_solution_documents = []
+    incident_solution_ids = []
+
+    playbook_documents = []
+    playbook_ids = []
+
+    policy_documents = []
+    policy_ids = []
+
+    security_history_documents = []
+    security_history_ids = []
+
+    # READ ALL JSON FILES
+    with open("company-data/incidents.json", "r", encoding="utf-8") as f:
         incident_data = json.load(f)
+    with open("company-data/incident-solutions.json", "r", encoding="utf-8") as f:
+        incident_solution_data = json.load(f)
+    with open("company-data/playbooks.json", "r", encoding="utf-8") as f:
+        playbook_data = json.load(f)
+    with open("company-data/policies.json", "r", encoding="utf-8") as f:
+        policy_data = json.load(f)
+    with open("company-data/security-history.json", "r", encoding="utf-8") as f:
+        security_history_data = json.load(f)
 
-    for incident in incident_data["incidents"]:
+    # ITERATE THROUGH ALL JSON DATA FILES
+    for incident in incident_data["data"]:
         document = Document(
             page_content=f"""
+                Incident ID: {incident["id"]}
                 Title: {incident["title"]}
+                Date: {incident["date"]}
+                Severity: {incident["severity"]}
+                Category: {incident["category"]}
+                Status: {incident["status"]}
                 Affected systems: {incident["affected_systems"]}
                 Indicators: {incident["indicators"]}
                 Summary: {incident["summary"]}
@@ -31,6 +90,7 @@ if add_documents:
                 Response: {incident["response"]}
                 Outcome: {incident["outcome"]}
                 Lessons learned: {incident['lessons_learned']}
+                
             """,
             metadata={
                 "incident_id": incident["id"],
@@ -41,19 +101,123 @@ if add_documents:
             },
             id=incident["id"],
         )
-        ids.append(incident["id"])
-        documents.append(document)
+        incident_ids.append(incident["id"])
+        incident_documents.append(document)
 
-vector_store = Chroma(
-    collection_name="incident_history",
-    persist_directory=db_location,
-    embedding_function=embeddings,
+    for solution in incident_solution_data["data"]:
+        document = Document(
+            page_content=f"""
+                Solution ID: {solution["id"]}
+                Incident ID: {solution["incident_id"]}
+                Incident type: {solution["incident_type"]}
+                Problem: {solution["problem"]}
+                Investigation approach: {solution["investigation_approach"]}
+                Actions taken: {solution["actions_taken"]}
+                Result: {solution["result"]}
+                Effectiveness: {solution["effectiveness"]}
+                Lessons learned: {solution["lessons_learned"]}
+                
+            """,
+            metadata={
+                "solution_id": solution["id"],
+                "incident_id": solution["incident_id"],
+                "effectiveness": solution["effectiveness"],
+            },
+            id=solution["id"],
+        )
+        incident_solution_ids.append(solution["id"])
+        incident_solution_documents.append(document)
+
+    for playbook in playbook_data["data"]:
+        document = Document(
+            page_content=f"""
+                Playbook ID: {playbook["id"]}
+                Name: {playbook["name"]}
+                Version: {playbook["version"]}
+                Owner: {playbook["owner"]}
+                Severity: {playbook["severity"]}
+                Trigger conditions: {playbook["trigger_conditions"]}
+                Initial actions: {playbook["initial_actions"]}
+                Investigation: {playbook["investigation"]}
+                Containment: {playbook["containment"]}
+                Escalation: {playbook["escalation"]}
+                Related incidents: {playbook["related_incidents"]}
+            """,
+            metadata={
+                "playbook_id": playbook["id"],
+                "version": playbook["version"],
+                "owner": playbook["owner"],
+            },
+            id=playbook["id"],
+        )
+        playbook_ids.append(playbook["id"])
+        playbook_documents.append(document)
+
+    for policy in policy_data["data"]:
+        document = Document(
+            page_content=f"""
+                Policy ID: {policy["id"]}
+                Name: {policy["name"]}
+                Version: {policy["version"]}
+                Requirements: {policy["requirements"]}
+                Monitoring_requirements: {policy["monitoring_requirements"]}
+                Exceptions: {policy["exceptions"]}
+                Related incidents: {policy["related_incidents"]}
+            """,
+            metadata={"id": policy["id"], "version": policy["version"]},
+            id=policy["id"],
+        )
+
+        policy_ids.append(policy["id"])
+        policy_documents.append(document)
+
+    for history_record in security_history_data["data"]:
+        document = Document(
+            page_content=f"""
+                History Record ID: {history_record["id"]}
+                Period: {history_record["period"]}
+                Total incidents: {history_record["total_incidents"]}
+                Critical incidents: {history_record["critical_incidents"]}
+                High incidents: {history_record["high_incidents"]}
+                Medium incidents: {history_record["medium_incidents"]}
+                Phishing incidents: {history_record["phishing_incidents"]}
+                Malware incidents: {history_record["malware_incidents"]}
+                Credential incidents: {history_record["credential_incidents"]}
+                Mean time to detect (hours): {history_record["mean_time_to_detect_hours"]}
+                Mean time to respond (hours): {history_record["mean_time_to_respond_hours"]}
+                False positive rate: {history_record["false_positive_rate"]}
+                Major observations: {history_record["major_observations"]}
+                Top categories: {history_record["top_categories"]}
+                Related incidents: {history_record["related_incidents"]}
+            """,
+            metadata={"id": history_record["id"], "period": history_record["period"]},
+            id=history_record["id"],
+        )
+
+        security_history_ids.append(history_record["id"])
+        security_history_documents.append(document)
+
+    # ADD ALL DOCUMENTS TO RELEVANT VECTOR STORES
+    incident_store.add_documents(documents=incident_documents, ids=incident_ids)
+
+    incident_solution_store.add_documents(
+        documents=incident_solution_documents, ids=incident_solution_ids
+    )
+    playbook_store.add_documents(documents=playbook_documents, ids=playbook_ids)
+
+    policy_store.add_documents(documents=policy_documents, ids=policy_ids)
+
+    security_history_store.add_documents(
+        documents=security_history_documents, ids=security_history_ids
+    )
+
+# CREATE THE RETRIEVERS
+
+# search_kwargs={"k": 5} means return 5 results
+incident_retriever = incident_store.as_retriever(search_kwargs={"k": 5})
+incident_solution_retriever = incident_solution_store.as_retriever(
+    search_kwargs={"k": 5}
 )
-
-if add_documents:
-    vector_store.add_documents(documents=documents, ids=ids)
-
-number_of_incidents_to_return = 10
-retriever = vector_store.as_retriever(
-    search_kwargs={"k": number_of_incidents_to_return}
-)
+playbook_retriever = playbook_store.as_retriever(search_kwargs={"k": 5})
+policy_retriever = policy_store.as_retriever(search_kwargs={"k": 5})
+security_history_retriever = security_history_store.as_retriever(search_kwargs={"k": 5})
